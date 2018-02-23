644a868a44af26f2d73c5482c564a3971855b2ed
Banke <fb@tigermedia.dk>
2018-02-23 07:45:54 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SpyLib;
+
+namespace SpyGame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyClass foo = new MyClass();
+        }
+    }
+}
---
+++ SpyLib/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SpyLib
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine("Hello Class!");
+        }
+        public int Divide(int number1, int number2)
+        {
+            int result;
+            try
+            {
+                result = number1 / number2;
+            }
+            catch (DivideByZeroException)
+            {
+                result = -1;
+            }
+            return result;
+        }
+    }
+}
