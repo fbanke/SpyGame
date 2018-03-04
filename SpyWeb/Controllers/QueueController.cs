fcd90df4dcbfe85cf5573ea72198fd72f7a7b0ab
Banke <fb@tigermedia.dk>
2018-03-04 14:41:46 +0100

    test

--- SpyWeb/Controllers/QueueController.cs
+++ SpyWeb/Controllers/QueueController.cs
@@ -2,15 +2,14 @@
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 
-namespace SpyWeb
+namespace SpyWeb.Controllers
 {
     public class QueueController : Controller
     {
         // GET
         public IActionResult Index()
         {
-            return
-            View();
+            return View();
         }
         
         public ActionResult CreateQueue()
@@ -29,14 +28,3 @@ namespace SpyWeb
         }
     }
 }
-
-//CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-//GetEnvironmentVariable("STORAGE_CONNECTION"));
-//            
-//CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
-//
-//// Retrieve a reference to a container.
-//CloudQueue queue = queueClient.GetQueueReference("spyQueue");
-//
-//// Create the queue if it doesn't already exist
-//queue.CreateIfNotExistsAsync();
