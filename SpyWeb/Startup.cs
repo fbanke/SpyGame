861dbf5ab42175efc0a6ae5fb064f91dbd71da4d
Banke <fb@tigermedia.dk>
2018-03-04 16:43:23 +0100

    test

--- SpyWeb/Startup.cs
+++ SpyWeb/Startup.cs
@@ -1,7 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace SpyWeb
 {
@@ -36,6 +40,28 @@ namespace SpyWeb
             app.UseStaticFiles();
 
             app.UseMvcWithDefaultRoute();
+
+            Setup();
+        }
+
+        public static CloudQueue SpyQueue { get; private set; }
+
+        public void Setup()
+        {
+            string connection = "";
+            try
+            {
+                connection = CloudConfigurationManager.GetSetting("AzureStorageConnectionString");
+            }
+            catch (Exception e)
+            {
+                connection = "UseDevelopmentStorage=true";
+            }
+            var storageAccount = CloudStorageAccount.Parse(connection);
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            
+            SpyQueue = queueClient.GetQueueReference("spy-queue");
+            SpyQueue.CreateIfNotExistsAsync();
         }
     }
 }
